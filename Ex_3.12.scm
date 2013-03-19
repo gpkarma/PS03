db7a0591a411086608764bf4b7b3ec23cc40d9b4
gpkarma <ksmith22@brandeis.edu>
2013-03-19 01:05:44 -0400

    aSF
    afewFQWEV

---
+++ Ex_3.12.scm
@@ -0,0 +1,23 @@
+#lang racket
+;;@author Kristen Smith
+;;Assignment: PS03, exercise 3.12
+;;@version 1.0
+;;date: 3/07/13
+
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
